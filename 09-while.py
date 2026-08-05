c9e87f57da3a74f3b99cfacb0705521c56e7b863
Samvel Tomeyan <tomeyansam@gmail.com>
2026-08-05 16:23:32 +0300

    task on python

--- 09-while.py
+++ 09-while.py
@@ -96,3 +96,29 @@ while n >= 1:
     n -= 1
     count += 1  
 print(count)
+
+num = 1576
+has_seven = False                                 # сигнальная метка (флаг)
+while num != 0:
+    last_digit = num % 10
+    if last_digit == 7:
+        has_seven = True
+    num = num // 10
+
+if has_seven == True:
+    print('YES')
+else:
+    print('NO')
+
+num = 8619
+n = len(str(num))
+for i in range(1, n + 1):
+    digit = num // 10 ** (n - i) % 10
+    print(i, '-я', ' цифра равна ', digit, sep='')
+
+num = 586
+while num > 0:
+    last_digit = num % 10
+    print(last_digit, sep='*', end='#')
+    num //= 10
+    print()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,4 +1,3 @@
-name = input()
-while name != 'Валера' and name != 'Артур':
-    print('Доступ запрещен')
-    name = input()
+h1, m1, h2, m2 = int(input()), int(input()), int(input()), int(input())
+while m1 <= m2:
+    print(f'{h1}:{m1 + 1}')

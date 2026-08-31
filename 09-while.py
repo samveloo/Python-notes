6fce4cc0768cd1eabc72f3b55261003a8621206f
Samvel <tomeyansam@gmail.com>
2026-08-31 13:41:34 +0300

    solve a task on stepik.org

--- 09-while.py
+++ 09-while.py
@@ -182,3 +182,9 @@ while n > 0:
     n //= 10
         
 print(flag)
+
+n = int(input())
+for i in range(1, n + 1):
+    if 5 <= i <= 9 or 17 <= i <= 37 or 78 <= i <= 87:
+        continue
+    print(i)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,6 +1,2 @@
-num = 586
-while num > 0:
-    last_digit = num % 10
-    print(last_digit, sep='*', end='#')
-    num //= 10
-    print()
+
+print(3%9)

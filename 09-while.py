3abd05d66a34a80c3f909bb9f60df2a63ebbc92e
Samvel <tomeyansam@gmail.com>
2026-08-31 13:41:54 +0300

    solve 2 tasks on stepik.org

--- 09-while.py
+++ 09-while.py
@@ -188,3 +188,9 @@ for i in range(1, n + 1):
     if 5 <= i <= 9 or 17 <= i <= 37 or 78 <= i <= 87:
         continue
     print(i)
+
+n = int(input())
+for i in range(2, n + 1):
+    if not (n % i):
+        print(i)
+        break

076bab1c0018b4e4f316d668cd28f512d3b596f6
Samvel Tomeyan <tomeyansam@gmail.com>
2026-09-17 14:25:13 +0300

    solve 3 tasks at stepik.org

--- 09-while.py
+++ 09-while.py
@@ -226,4 +226,10 @@ n = int(input())
 for i in range(1, n + 1):
     for j in range(5):
         print(i, end=' ')
-    print() 
+    print()
+
+n = int(input())
+for i in range(1, n + 1):
+    for j in range(1, 10):
+        print(f"{i} + {j} = {i + j}")
+    print()

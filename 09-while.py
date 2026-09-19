65023f6a6418da5317fc65eb841bbffb15aadfa3
Samvel Tomeyan <tomeyansam@gmail.com>
2026-09-19 22:01:56 +0300

    x =

--- 09-while.py
+++ 09-while.py
@@ -238,3 +238,11 @@ for i in range(1, n := int(input()) + 1):
     for j in range(i):
         print(i, end='')
     print()
+
+total = 0
+for x in range(1, 65):
+    for y in range(1, 60):
+        if 12 * x + 13 * y == 777:
+            total += 1
+            print('x =', x, 'y =', y)
+print('Общее количество натуральных решений =', total)
---
+++ xravno.py
@@ -0,0 +1,7 @@
+total = 0
+for x in range(1, 65):
+    for y in range(1, 60):
+        if 12 * x + 13 * y == 777:
+            total += 1
+            print('x =', x, 'y =', y)
+print('Общее количество натуральных решений =', total)
